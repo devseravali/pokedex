5364ca2c6d5316c00f35c46b069e61be49666add
devseravali <devseravali@gmail.com>
2025-11-05 02:24:46 -0300

    Melhorias para deploy

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import ReactDOM from 'react-dom/client';
 import App from './App';
-ReactDOM.createRoot(document.getElementById('root')).render(_jsx(App, {}));
+import { BrowserRouter } from 'react-router-dom';
+ReactDOM.createRoot(document.getElementById('root')).render(_jsx(BrowserRouter, { basename: "/pokedex", children: _jsx(App, {}) }));

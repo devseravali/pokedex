bc340ec67b09649e8f655fbe80715769f744165e
devseravali <devseravali@gmail.com>
2025-11-05 00:47:51 -0300

    Melhoria para teste

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
    moduleNameMapper: {
      '\\.(css|less|scss|sass)$': 'identity-obj-proxy', 
      '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
-     '\\.(ttf|woff|woff2|eot|otf)$': '<rootDir>/__mocks__/fileMock.js',
+     '\\.ttf$': '<rootDir>/__mocks__/fileMock.js',
    },
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'], 
   testPathIgnorePatterns: ['/node_modules/', '/dist/'], 

0cd87c054f7eeaf8e5510a36738261490954f56e
devseravali <devseravali@gmail.com>
2025-11-05 01:57:49 -0300

    Atualização para deploy

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
+import App from './App';
 import React from 'react';
 createRoot(document.getElementById('root')).render(_jsx(React.StrictMode, { children: _jsx(BrowserRouter, { children: _jsx(App, {}) }) }));

8bd4d474ffd50c65f5fa5012b2fcb40e42b3fba0
devseravali <devseravali@gmail.com>
2025-11-06 00:00:11 -0300

    Ajusta tipos de assets e corrige build"

--- src/styles/MediaQueries.js
+++ src/styles/MediaQueries.js
@@ -1,9 +1,6 @@
 export const MediaQueries = {
     mobile: '@media (max-width: 480px)',
-  
     tablet: '@media (min-width: 481px) and (max-width: 1024px)',
-  
     desktop: '@media (min-width: 1025px) and (max-width: 1919px)',
-
     large: '@media (min-width: 1920px)',
-  };
+};
